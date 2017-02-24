b8417e222c806a0d11121ed6043ca1609d5b1d4a
Yedhu Krishnan <yedhukrishnan@yahoo.com>
2017-02-24 13:04:46 +0530

    Initial Commit

---
+++ onemdm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onemdm
+//
+//  Created by Yedhu Krishnan on 24/02/17.
+//  Copyright © 2017 Multunus Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
